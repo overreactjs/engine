e6076cb169b54d68d0ddf0c87908be6ee044ddbe
Craig Smith <craig@craigsmith.info>
2024-01-03 14:23:01 +0000

    Fixed camera smoothing to be FPS agnostic

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -1,8 +1,8 @@
-import { useDebug, useElement, usePosition, useRender, useViewport } from "../hooks";
+import { useDebug, useElement, usePosition, useRender, useUpdate, useViewport } from "../hooks";
 import { CameraAxis, Position } from "../types";
 import { lerp } from "../utils";
 
-const SMOOTH_FACTOR = 0.04;
+const SMOOTH_FACTOR = 0.0048;
 
 const CLASS_NAME = "absolute w-2 h-2 -m-1 border-2 border-pink-600";
 
@@ -25,22 +25,22 @@ export const Camera: React.FC<CameraProps> = ({ axis = 'xy', offset = [0, 0], sm
   const { origin } = useViewport();
   const pos = usePosition();
 
-  useRender(() => {
-    // TEMPORARY HACK: WE NEED THE CAMERA POSITION TO UPDATE LAST, SO IT'S BEEN ADDED TO THE RENDER
-    // FUNCTION, EVEN THOUGH IT BELONGS IN THE UPDATE FUNCTION.
+  useUpdate((delta) => {
     if (origin) {
       const newOrigin: Position = [origin.current[0], origin.current[1]];
 
       if (axis === 'x' || axis === 'xy') {
-        newOrigin[0] = lerp(origin.current[0], pos.current[0] + offset[0], smooth ? SMOOTH_FACTOR : 1);
+        newOrigin[0] = lerp(origin.current[0], pos.current[0] + offset[0], smooth ? SMOOTH_FACTOR * delta : 1);
       }
       if (axis === 'y' || axis === 'xy') {
-        newOrigin[1] = lerp(origin.current[1], pos.current[1] + offset[1], smooth ? SMOOTH_FACTOR : 1);
+        newOrigin[1] = lerp(origin.current[1], pos.current[1] + offset[1], smooth ? SMOOTH_FACTOR * delta : 1);
       }
 
       origin.current = newOrigin;
     }
+  })
 
+  useRender(() => {
     if (debug) {
       element.setBaseStyles({ pos: origin });
     }
