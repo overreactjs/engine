d5eb20212ba66ee64575f5991c328ed3438c2dc9
Craig Smith <craig@craigsmith.info>
2024-02-07 16:20:37 +0000

    Added 'active' parameter to useKeyboardMap.

--- src/hooks/useKeyboardMap.ts
+++ src/hooks/useKeyboardMap.ts
@@ -1,19 +1,25 @@
+import { useProperty } from ".";
+import { Prop } from "../types";
 import { useKeyboard } from "./useKeyboard";
 import { useUpdate } from "./useUpdate";
 import { useVirtualInput } from "./useVirtualInput";
 
 type KeyboardMap = Record<string, string | null>;
 
-export const useKeyboardMap = (map: KeyboardMap) => {
+export const useKeyboardMap = (map: KeyboardMap, active?: Prop<boolean>) => {
   const { simulate } = useVirtualInput();
   const { isKeyDown } = useKeyboard();
 
+  const isActive = useProperty(active === undefined ? true : active);
+
   useUpdate(() => {
-    for (const action in map) {
-      const key = map[action];
-      
-      if (key !== null && isKeyDown(key)) {
-        simulate(action);
+    if (isActive.current) {
+      for (const action in map) {
+        const key = map[action];
+        
+        if (key !== null && isKeyDown(key)) {
+          simulate(action);
+        }
       }
     }
   });
