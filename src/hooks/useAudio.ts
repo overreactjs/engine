3384f17f0c944074647a99d85fb8b0abb9d9d572
Craig Smith <craig@craigsmith.info>
2024-03-07 14:37:51 +0000

    Setup AudioContext once on initial render.

--- src/hooks/useAudio.ts
+++ src/hooks/useAudio.ts
@@ -1,14 +1,18 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useEffect, useMemo, useRef } from "react";
 
 type UseAudioResult = {
   play: (url: string) => void;
 };
 
 export const useAudio = (): UseAudioResult => {
+  const context = useRef<AudioContext>();
+
+  useEffect(() => {
+    const AudioContext = window.AudioContext || (window as any).webkitAudioContext;
+    context.current = new AudioContext();
+  }, []);
 
   const play = useCallback((path: string) => {
-    const AudioContext = window.AudioContext || (window as any).webkitAudioContext;
-    new AudioContext();
     const audio = new Audio(path);
     audio.play();
   }, []);
